7e0fb94fa43b5b68797c21d1e41e8158f7716c7c
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-05-11 10:30:30 +0530

    variable

--- 02variables/main.go
+++ 02variables/main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("variable")
+
+	// string
+	var stringVariable string
+	fmt.Println(stringVariable)
+	fmt.Printf("Variable is of type : %T \n", stringVariable)
 }
