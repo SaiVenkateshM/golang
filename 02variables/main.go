58699e1a3459c42588559ac2937c5952f49b8207
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-05-17 12:53:31 +0530

    16th May

--- 02variables/main.go
+++ 02variables/main.go
@@ -1,6 +1,8 @@
-package variables
+package main
 
 import "fmt"
+
+var Login string = "10000"
 
 func main() {
 	fmt.Println("variable")
@@ -9,4 +11,6 @@ func main() {
 	var stringVariable string
 	fmt.Println(stringVariable)
 	fmt.Printf("Variable is of type : %T \n", stringVariable)
+
+	fmt.Printf("Login is of type : %T \n", Login)
 }
