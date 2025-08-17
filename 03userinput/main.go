7edd37e21487162345804e5ff077c28ede898403
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-08-17 13:15:27 +0530

    conversion

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	welocomeMessage := "Welcome to the user input program"
 	fmt.Println(welocomeMessage)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your name: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Hello, ", input)
 }
