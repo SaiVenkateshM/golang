c6685cfb707f3153ab5a7964af21f04b0b569c33
saivenkateshmaruvada <saivenkatesh1403@gmail.com>
2025-08-17 12:15:54 +0530

    boolean example

--- 02variables/main.go
+++ 02variables/main.go
@@ -9,8 +9,9 @@ func main() {
 
 	// string
 	var stringVariable string
+	var isLoggedIn bool = true
 	fmt.Println(stringVariable)
 	fmt.Printf("Variable is of type : %T \n", stringVariable)
-
 	fmt.Printf("Login is of type : %T \n", Login)
+	fmt.Printf("isLoggedIn is of type : %T \n", isLoggedIn)
 }
